6273108a9bfe08076d2492183ef98704f488394f
GUNKWIN <2512690268@qq.com>
2022-12-04 12:26:46 +0800

    FEAT: 尝试复刻Lab5场景，但目前还不行

--- include/mesh.h
+++ include/mesh.h
@@ -65,7 +65,7 @@ public:
                         const std::vector<unsigned int>& Indices,
                         unsigned int MaterialIndex);
 
-private:
+protected:
     bool InitFromScene(const aiScene* pScene, const std::string& Filename);
     void InitMesh(unsigned int Index, const aiMesh* paiMesh);
     bool InitMaterials(const aiScene* pScene, const std::string& Filename);
@@ -75,5 +75,39 @@ private:
     std::vector<Texture*> m_Textures;
 };
 
+class CubicMesh : public Mesh
+{
+public:
+    CubicMesh(){
+        std::vector<Vertex> vertices = {
+            Vertex(glm::vec3(-1.0f, -1.0f, -1.0f), glm::vec2(0.0f, 0.0f)),
+            Vertex(glm::vec3(-1.0f, -1.0f,  1.0f), glm::vec2(0.0f, 1.0f)),
+            Vertex(glm::vec3(-1.0f,  1.0f, -1.0f), glm::vec2(1.0f, 0.0f)),
+            Vertex(glm::vec3(-1.0f,  1.0f,  1.0f), glm::vec2(1.0f, 1.0f)),
+            Vertex(glm::vec3( 1.0f, -1.0f, -1.0f), glm::vec2(0.0f, 0.0f)),
+            Vertex(glm::vec3( 1.0f, -1.0f,  1.0f), glm::vec2(0.0f, 1.0f)),
+            Vertex(glm::vec3( 1.0f,  1.0f, -1.0f), glm::vec2(1.0f, 0.0f))
+        };
+
+        std::vector<unsigned int> indices = {
+            0, 1, 2,
+            2, 1, 3,
+            4, 6, 5,
+            5, 6, 7,
+            0, 2, 4,
+            4, 2, 6,
+            1, 5, 3,
+            3, 5, 7,
+            0, 4, 1,
+            1, 4, 5,
+            2, 3, 6,
+            6, 3, 7
+        };
+
+        m_Textures.push_back(nullptr);
+        AddMeshEntry(vertices, indices, 0);
+
+    }
+};
 
 #endif  /* MESH_H */
